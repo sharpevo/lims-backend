27e240de8f019ff9f8ecf28288e9bc5c30fde9f8
sharpevo <sharpevo@outlook.com>
2017-03-26 16:43:47 +0800

    Remove useless files

--- app/routes/index.server.routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-  const index = require('../contrllers/index.server.controller')
-  app.get('/*', index.render)
-}
-
--- app/routes/users.server.routes.js
+++
@@ -1,5 +0,0 @@
-const users = require('../../app/controllers/users.server.controller')
-
-module.exports = function(app) {
-  app.route('/users').post(users.create)
-}
