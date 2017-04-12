fa63d04931fe3a3fcde640faa04338b0ee76a776
sharpevo <sharpevo@outlook.com>
2017-04-12 11:17:19 +0800

    Change list method in the genre controller

--- app/controllers/genre.js
+++ app/controllers/genre.js
@@ -1,6 +1,7 @@
 const Genre = require('mongoose').model('Genre')
 const Entity = require('mongoose').model('Entity')
 const Attribute = require('mongoose').model('Attribute')
+const Utils = require('../utils/controller')
 
 exports.create = function(req, res, next){
     const genre = new Genre(req.body)
@@ -16,21 +17,10 @@ exports.create = function(req, res, next){
 }
 
 exports.list = function(req, res, next){
-    let options = {}
-    if (req.query.limit){
-        options["limit"] = parseInt(req.query.limit)
-        delete req.query["limit"]
-    }
-    if (req.query.skip){
-        options["skip"] = parseInt(req.query.skip)
-        delete req.query["skip"]
-    }
-
-    Genre.find(
-        req.query,
-        '',
-        options,
-        (err, genres) => {
+    let query = Utils.list(req, Genre)
+    query
+        .populate('SYS_ENTITY')
+        .exec((err, genres) => {
             if (err){
                 return res.status(400).send({
                     message: parseError(err)
@@ -39,7 +29,6 @@ exports.list = function(req, res, next){
                 res.status(200).json(genres)
             }
         })
-        .populate('SYS_ENTITY')
 }
 
 // Actions with ID specified
