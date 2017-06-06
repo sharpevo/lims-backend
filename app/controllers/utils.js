a83a81de50e3029c5377fb1a06e971e118b2ba58
sharpevo <sharpevo@outlook.com>
2017-06-06 10:25:14 +0800

    Add excel export

--- app/controllers/utils.js
+++ app/controllers/utils.js
@@ -1,6 +1,9 @@
+const Attribute = require('mongoose').model('Attribute')
+const Genre = require('mongoose').model('Genre')
+const Entity = require('mongoose').model('Entity')
 
 var formidable = require('formidable')
-var XLSX = require('xlsx');
+var XLSX = require('xlsx')
 
 exports.excelToJSON = function(req, res, next){
     var form = new formidable.IncomingForm()
@@ -13,63 +16,136 @@ exports.excelToJSON = function(req, res, next){
 
         var workbook = XLSX.readFile(file.path)
         //var workbook = XLSX.read(file, {type:'binary'})
-        var sheet_name_list = workbook.SheetNames;
+        var sheet_name_list = workbook.SheetNames
         var jsonObject = []
         sheet_name_list.forEach(function(y) {
-            var worksheet = workbook.Sheets[y];
-            var headers = {};
-            var data = [];
+            var worksheet = workbook.Sheets[y]
+            var headers = {}
+            var data = []
             for(z in worksheet) {
-                if(z[0] === '!') continue;
+                if(z[0] === '!') continue
                 //parse out the column, row, and value
-                var tt = 0;
+                var tt = 0
                 for (var i = 0; i < z.length; i++) {
                     if (!isNaN(z[i])) {
-                        tt = i;
-                        break;
+                        tt = i
+                        break
                     }
-                };
+                }
 
-                var col = z.substring(0,tt);
-                var row = parseInt(z.substring(tt));
-                var value = worksheet[z].v;
+                var col = z.substring(0,tt)
+                var row = parseInt(z.substring(tt))
+                var value = worksheet[z].v
                 //console.log(col)
                 //console.log(row)
                 //console.log(value)
 
                 //store header names
                 if(row == 2 && value) {
-                    headers[col] = value;
-                    continue;
+                    headers[col] = value
+                    continue
                 }
 
-                if(!data[row]) data[row]={};
-                data[row][headers[col]] = value;
+                if(!data[row]) data[row]={}
+                data[row][headers[col]] = value
             }
             //drop those first two rows which are empty
-            data.shift();
-            data.shift();
+            data.shift()
+            data.shift()
             jsonObject.push(data)
-        });
+        })
         res.status(200).json(jsonObject)
 
     })
-    //var form = new formidable.IncomingForm();
+    //var form = new formidable.IncomingForm()
 
-    //form.parse(req);
+    //form.parse(req)
 
     //form.on('fileBegin', function (name, file){
-    //file.path = '/home/yang/nodejs/igenetech/lims-backend/uploads/' + file.name;
-    //});
+    //file.path = '/home/yang/nodejs/igenetech/lims-backend/uploads/' + file.name
+    //})
 
     //form.on('file', function (name, file){
-    //console.log('Uploaded ' + file.name);
-    //});
+    //console.log('Uploaded ' + file.name)
+    //})
 
-    ////res.sendFile(__dirname + '/index.html');
+    ////res.sendFile(__dirname + '/index.html')
     //res.status(200).json("uploaded")
 }
 
 exports.JSONToExcel = function(req, res, next){
-    res.status(200).json("json to excel")
+    if (!req.query.ids){
+        console.log('invalid ids')
+        res.status(400).json('invalid arguments')
+        return
+    }
+
+    let ids = req.query.ids.split(',')
+
+    let entity = Entity.findOne(
+        {_id: ids[0]},
+        (err, entity) => {
+            if (!entity){
+                console.log(entity)
+                res.status(400).json('invalid id')
+                return
+            }
+            Attribute.find(
+                {'SYS_GENRE': entity.SYS_GENRE},
+                '',
+                {
+                    sort:{
+                        SYS_ORDER: 1
+                    }
+                },
+                (err, attributes) => {
+                    let headers = []
+                    let fields = []
+                    attributes.forEach(attribute => {
+                        let attr = JSON.parse(JSON.stringify(attribute))
+                        headers.push(attr[attr['SYS_LABEL']])
+                        fields.push(attr['SYS_CODE'])
+                    })
+                    headers.push('IDENTIFIER')
+                    fields.push('id')
+
+                    let data = []
+                    Entity.find(
+                        {_id: {$in: ids}},
+                        (err, entities) => {
+                            entities.forEach(entity => {
+                                let e = JSON.parse(JSON.stringify(entity))
+                                let object = {}
+                                fields.forEach(key => {
+                                    object[key] = e[key]?e[key]:''
+                                })
+                                data.push(object)
+                            })
+
+                            let _headers = headers
+                                .map((v, i) => Object.assign({}, {v: v, position: String.fromCharCode(65+i) + 1 }))
+                                .reduce((prev, next) => Object.assign({}, prev, {[next.position]: {v: next.v}}), {})
+                            let _data = data
+                                .map((v, i) => fields.map((k, j) => Object.assign({}, { v: v[k], position: String.fromCharCode(65+j) + (i+2) })))
+                                .reduce((prev, next) => prev.concat(next))
+                                .reduce((prev, next) => Object.assign({}, prev, {[next.position]: {v: next.v}}), {})
+                            let output = Object.assign({}, _headers, _data)
+                            let outputPos = Object.keys(output)
+                            let ref = outputPos[0] + ':' + outputPos[outputPos.length - 1]
+                            let wb = {
+                                SheetNames: ['mySheet'],
+                                Sheets: {
+                                    'mySheet': Object.assign({}, output, { '!ref': ref })
+                                }
+                            }
+                            XLSX.writeFile(wb, 'sampleexport.xlsx')
+                        }
+                    )
+
+
+                }
+            )
+        }
+    )
+
 }
