f88cd348ee5bd885f4f8d02c01996f9e51183f2e
sharpevo <sharpevo@outlook.com>
2017-11-01 11:36:59 +0800

    Add setCookie and getUserInfo service
    Issue: #50

--- app/routes/utils.js
+++ app/routes/utils.js
@@ -1,6 +1,12 @@
 module.exports = function(app){
     const utils = require('../controllers/utils')
     app
+        .route('/')
+        .get(utils.setCookie)
+    app
+        .route('/userinfo')
+        .get(utils.getUserInfo)
+    app
         .route('/excel')
         .post(utils.JSONToExcel)
         .put(utils.updateInBatch)
