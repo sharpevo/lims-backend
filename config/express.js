63a793cc45e4e7a2ff44a45a04a481e917e69856
sharpevo <sharpevo@outlook.com>
2017-11-20 15:45:42 +0800

    Expose customized headers to CORS response
    Issue: #50

--- config/express.js
+++ config/express.js
@@ -24,6 +24,9 @@ module.exports = function() {
             res.header('Access-Control-Allow-Origin', req.headers.origin);
             res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Content-Type');
+            res.header(
+                'Access-Control-Expose-Headers',
+                'igenetech-user-id,igenetech-user-name,igenetech-user-email,igenetech-user-roles,igenetech-user-role,igenetech-user-limsid')
             res.header('Access-Control-Allow-Credentials', 'true')
         }
         next();
