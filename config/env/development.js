3f57e61b9f65f42c33f42a844b39a3baad20bf1d
sharpevo <sharpevo@outlook.com>
2017-11-21 08:09:41 +0800

    Add frontendUrl in configs

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
     db: 'mongodb://localhost:27017/mean-backend',
     corsOrigin: ['http://lims.igenetech.cn', 'http://localhost:8000'],
+    frontendUrl: 'http://localhost:8000',
     sessionSecret: 'developmentSessionSecret'
 }
