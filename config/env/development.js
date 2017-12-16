7d82a9f20c38bb6df8424755b42e8641ede94825
sharpevo <sharpevo@outlook.com>
2017-12-17 07:06:10 +0800

    Add corsOrigin

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: process.env.LIMS_DATABASE_ADDR || 'mongodb://localhost:27017/mean-backend',
-    corsOrigin: ['http://lims.igenetech.cn', process.env.LIMS_CORS_ORIGIN || 'http://localhost:8000'],
+    corsOrigin: ['http://lims.igenetech.cn', process.env.LIMS_CORS_ORIGIN || 'http://localhost:8000', 'http://59.108.63.4:8000'],
     frontendUrl: process.env.LIMS_FRONTEND_ADDR || 'http://localhost:8000',
     sessionSecret: process.env.LIMS_SESSION_SECRET || 'developmentSessionSecret'
 }
