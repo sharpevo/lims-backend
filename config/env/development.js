91be3ec8e1d91b20994c7a2ec15d8c27d8e3ec93
sharpevo <sharpevo@outlook.com>
2018-02-07 10:13:22 +0800

    Add audit environment variables
    Issue: #197

--- config/env/development.js
+++ config/env/development.js
@@ -2,5 +2,7 @@ module.exports = {
     db: process.env.LIMS_DATABASE_ADDR || 'mongodb://localhost:27017/mean-backend',
     corsOrigin: ['http://lims.igenetech.cn', process.env.LIMS_CORS_ORIGIN || 'http://localhost:8000', 'http://59.108.63.4:8000'],
     frontendUrl: process.env.LIMS_FRONTEND_ADDR || 'http://localhost:8000',
-    sessionSecret: process.env.LIMS_SESSION_SECRET || 'developmentSessionSecret'
+    sessionSecret: process.env.LIMS_SESSION_SECRET || 'developmentSessionSecret',
+    auditHost: "audit.lims.igenetech.cn",
+    auditPath: "/audit",
 }
--- config/env/test.js
+++ config/env/test.js
@@ -2,5 +2,7 @@ module.exports = {
     db: process.env.LIMS_DATABASE_ADDR || 'mongodb://localhost:27011/mean-backend',
     corsOrigin: ['http://lims.igenetech.cn', process.env.LIMS_CORS_ORIGIN || 'http://localhost:8000'],
     frontendUrl: process.env.LIMS_FRONTEND_ADDR || 'http://192.168.1.99:8000',
-    sessionSecret: process.env.LIMS_SESSION_SECRET || 'developmentSessionSecret'
+    sessionSecret: process.env.LIMS_SESSION_SECRET || 'developmentSessionSecret',
+    auditHost: "audit.lims.igenetech.cn",
+    auditPath: "/audit",
 }
