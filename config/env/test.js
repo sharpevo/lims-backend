d50744c8cacf9129d1690efc07f749e4b47e9362
sharpevo <sharpevo@outlook.com>
2018-02-08 16:27:43 +0800

    Change audit api

--- config/env/test.js
+++ config/env/test.js
@@ -3,6 +3,6 @@ module.exports = {
     corsOrigin: ['http://lims.igenetech.cn', process.env.LIMS_CORS_ORIGIN || 'http://localhost:8000'],
     frontendUrl: process.env.LIMS_FRONTEND_ADDR || 'http://192.168.1.99:8000',
     sessionSecret: process.env.LIMS_SESSION_SECRET || 'developmentSessionSecret',
-    auditHost: "audit.lims.igenetech.cn",
+    auditHost: "api.audit.lims.igenetech.cn",
     auditPath: "/audit",
 }
